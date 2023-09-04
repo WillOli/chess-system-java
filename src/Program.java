863e0bc2525f56106b15c333fe5476d408249e78
WillOli <william.ika@gmail.com>
2023-09-04 00:41:22 -0300

    First class: Position

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,10 @@
+import boargame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Position pos = new Position(2, 5);
+
+        System.out.println(pos);
     }
 }
